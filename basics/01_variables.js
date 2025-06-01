d2239d1c65cbf668a9cbaf430858c3eec2fc6c41
kannu-blip <kannu777a@gmail.com>
2025-06-01 14:59:51 +0000

    variables and dataypes

---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234668
+let accountEmail = "kannu@google.com"
+var accountPassword = "14637"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 53890 cannot be change
+accountEmail = "heykannu@google.com"
+accountPassword = "173973"
+accountCity = "roorkee"
+
+console.log(accountId);
+
+console.table([accountEmail ,accountId ,accountPassword ,accountCity ,accountState])
---
+++ basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict";    //treat all js code as newer version
+
+// alert(3+3) we're using nodejs not browser
+
+console.log(3+3)
+console.log("hlwieee")
+
+let name ="kannu"
+let age = 18
+let isLogedIn = false
+
+// datatypes :-
+/*
+Number = 2 to power 53
+bigint
+String = ""
+Boolean = true or false
+null = standalone values / empty value
+undefined = variale declared but value not given
+symbol = unique
+objects
+*/
+
+console.log(typeof undefined)
+console.log(typeof null)
