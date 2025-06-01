2297de812b6dc27287f0a285dfc6726001daa8bd
kannu-blip <kannu777a@gmail.com>
2025-06-01 16:12:30 +0000

    done

--- basics/03_conversionoperation.js
+++ basics/03_conversionoperation.js
@@ -25,5 +25,36 @@ let BooleanisLogedIn = Boolean(isLogedIn)
 
 let somenumber = 33
 let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
+
+/*                  operations                     */
+ 
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " kannu"
+
+str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
