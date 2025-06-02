a54e5f5f5c8966098c2a8d4b523f1963eb18a982
kannu-blip <kannu777a@gmail.com>
2025-06-02 06:56:30 +0000

    comapare and datatypes

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+/*****     primitive data types:-
+
+strings
+boolean 
+null
+numbers
+undefined
+symbol
+bigInt
+
+           Refrence data types:-
+objects
+arrays
+functions
+
+
+******/
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = true
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id == anotherId); // false
+
+// const bignumber = 66288587527928n
+
+let myobject = {
+    name: "kannu",
+    age : 18,
+}
+
+const myFunction = function() {
+    console.log("hello world");
+}
+
+console.log(typeof bignumber); //undefined
+console.log(typeof outsidetemp); //object
+console.log(typeof myFunction); //function
