8d3cac2acb8c2ee114e07206272a3868d4fafdfc
kannu-blip <kannu777a@gmail.com>
2025-06-08 15:35:43 +0000

    objects.2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,47 @@
+const tinderUser = new Object()                  //this is singleton object  
+// const tinderUser = {}  
+tinderUser.name = "kinoo" 
+tinderUser.Id = "001kanu"   
+tinderUser.isLoggedIn = false             
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "kannu@email.com",
+    fullname : {
+        userfullname :{
+            firstname : "kannu",
+            lastname : "kashyap"
+        }
+    }
+}
+// console.log(regularUser.fullname);    
+// console.log(regularUser.fullname.userfullname);    
+
+const obj1 = {1 :"a" , 2 :"b"}
+const obj2 = {3 :"a" , 4 :"b"}
+const obj3 = {5 :"a", 6 :"b"}
+// const obj4 = Object.assign({}, obj1 ,obj2 ,obj3)
+const obj4 = {...obj1 , ...obj2}
+// console.log(obj4);
+
+const Users = [
+    {
+        id : "kannu001",
+        email : "kannu@email.com"
+    },
+    {
+        id : "kannu001",
+        email : "kannu@email.com"
+    },
+    {
+        id : "kannu001",
+        email : "kannu@email.com"
+    }
+]
+// console.log(Users[1].email)
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLogged'));
