5838360de7961759a89ed5a8798f8e30cd423ef4
kannu-blip <kannu777a@gmail.com>
2025-09-04 15:53:24 +0000

    global & local scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,13 @@
+// var c = 100                  //global variable 
+let a = 300
+if (true){
+    let a = 10
+    const b = 20
+    console.log("Inner: ", a);
+    
+}
+for (let i = 0; i < array)
+
+console.log(a);
+// console.log(b);
+// console.log(c);
